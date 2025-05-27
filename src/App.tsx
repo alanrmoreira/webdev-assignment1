d5b5e73df3bd8b70e0f7fa8a04bdf6c2424301a3
alan <alan@nexxtideas.com>
2025-05-27 12:44:01 -0600

    build
    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ResponsiveAppBar from './components/Navbar';
 import Home from './Pages/Home';
